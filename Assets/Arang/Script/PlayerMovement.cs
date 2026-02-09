58f12675d9c1282971fab68e328617dcbbb7d1ca
RobyRAX <robyalexander571@gmail.com>
2026-02-09 07:47:22 +0700

    vertical camera rotation

--- Assets/Arang/Script/PlayerMovement.cs
+++ Assets/Arang/Script/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
     [Header("Setting")]
     public float moveSpeed = 5;
     public float horizontalRotateSpeed = 10f;
+    public float verticalRotateSpeed = 10f;
+
+    [Header("References")]
+    public Camera PlayerCamera;
     public Rigidbody RB;
 
     [SerializeField]
@@ -16,6 +20,11 @@ public class PlayerMovement : MonoBehaviour
         if (MouseDelta.x != 0)
         {
             transform.Rotate(Vector3.up, horizontalRotateSpeed * Time.deltaTime * MouseDelta.x);
+        }
+        
+        if (MouseDelta.y != 0)
+        {
+            PlayerCamera.transform.Rotate(Vector3.right, -verticalRotateSpeed * Time.deltaTime * MouseDelta.y);
         }
     }
 
